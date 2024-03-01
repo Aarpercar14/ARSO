bf9219006960af7a7da13c048ca86a9660d3a528
aaronperona <aarpercar@gmail.com>
2024-03-01 21:27:11 +0100

    Primera implementacion controladorRest

--- alquileres/src/main/java/alquileres/rest/AlquilerControladorRest.java
+++ alquileres/src/main/java/alquileres/rest/AlquilerControladorRest.java
@@ -1,36 +1,63 @@
 package alquileres.rest;
-import java.net.URI;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
-import java.util.LinkedList;
-import java.util.List;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
+
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 import alquileres.servicio.IServicioAlquileres;
 import servicio.FactoriaServicios;
 
 @Path("alquileres")
 public class AlquilerControladorRest {
+	
     private IServicioAlquileres servicio =  FactoriaServicios.getServicio(IServicioAlquileres.class);
     
     @POST
-    @Path("/{idUsuario}/{idBicicleta}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getActividad( @PathParam("idUsuario") String idUsuario, @PathParam("idBicicleta") String idBicicleta)
-    		throws Exception {
-    	   return Response.status(Response.Status.OK)
-    	                  .entity(servicio.reservar(idUsuario,idBicicleta)).build();
+    @Path("/usuarios/{idUsuario}/reservas/{idBicicleta}")
+    public Response reservar( @PathParam("idUsuario") String idUsuario, 
+    							@PathParam("idBicicleta") String idBicicleta)
+    									throws Exception {
+    	
+    		servicio.reservar(idUsuario, idBicicleta);   		
+    		return Response.status(Response.Status.NO_CONTENT).build();
+    		
     	}
+    
+    @POST
+    @Path("/usuarios/{idUsuario}/reserva/confirmar")
+    public Response confirmarReserva(@PathParam("idUsuario") String idUsuario) throws Exception {
+    	servicio.confirmarReserva(idUsuario);
+    	return Response.status(Response.Status.NO_CONTENT).build();
+    }
+    
+    @POST
+    @Path("/usuarios/{idUsuario}/alquileres/{idBicicleta}")
+    public Response alquilar(@PathParam("idUsuario") String idUsuario,
+    									@PathParam("idBicicleta") String idBicicleta)
+    											throws Exception {
+    	servicio.alquilar(idUsuario, idBicicleta);
+    	return Response.status(Response.Status.NO_CONTENT).build();
+    }
+    
+    @POST
+    @Path("/usuarios/{idUsuario}/desbloquear")
+    public Response desbloquearUsuario(@PathParam("idUsuario") String idUsuario) throws Exception {
+    	servicio.liberarBloqueo(idUsuario);
+    	return Response.status(Response.Status.NO_CONTENT).build();
+    }
+    
+    @GET
+    @Path("/usuarios/{idUsuario}/historial")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getHistorialUsuario(@PathParam("idUsuario") String idUsuario) throws Exception {
+    	return Response.status(Response.Status.OK)
+    			.entity(servicio.historialUsuario(idUsuario)).build();
+    }
+    
+    
 }
