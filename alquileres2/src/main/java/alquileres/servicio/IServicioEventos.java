5c7b44051effbc9ab4d6a3e993e79c9792e5313b
aaronperona <aarpercar@gmail.com>
2024-04-10 16:43:06 +0200

    Primera adaptacion servicio

--- alquileres2/src/main/java/alquileres/servicio/IServicioEventos.java
+++ alquileres2/src/main/java/alquileres/servicio/IServicioEventos.java
@@ -1,8 +1,8 @@
 package alquileres.servicio;
 
 public interface IServicioEventos {
-	void publicarEventoAlquilerConcluido(String idBici);
-	void publicarEventoBicicletaAlquilada(String idBici);
+	void publicarEventoAlquilerConcluido(String idBici) throws Exception;
+	void publicarEventoBicicletaAlquilada(String idBici)throws Exception;
 	void publicarEventoBicicletaDesactivada(String idBici);
 	void suscribirEventoAlquilerConcluido();
 	void suscribirEventoBicicletaAlquilada();
--- alquileres2/src/main/java/alquileres/servicio/ServicioEventos.java
+++ alquileres2/src/main/java/alquileres/servicio/ServicioEventos.java
@@ -4,6 +4,12 @@ import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 import alquileres.eventos.EventoAlquilerConcluido;
 import alquileres.eventos.EventoBicicletaAlquilada;
 import estaciones.eventos.EventoDesactivarBicicleta;
@@ -15,15 +21,65 @@ public class ServicioEventos implements IServicioEventos {
 	private PublicadorEventos publicador;
 
 	@Override
-	public void publicarEventoAlquilerConcluido(String idBici) {
+	public void publicarEventoAlquilerConcluido(String idBici) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setUri("amqps://ppvihfpt:BDGv28w-UU11pgRbRFJTStZyrY0L77Yd@stingray.rmq.cloudamqp.com/ppvihfpt");
+
+		Connection connection = factory.newConnection();
+		
+		Channel channel = connection.createChannel();
+		
+		final String exchangeName = "citybike";
+		boolean durable = true;
+		channel.exchangeDeclare(exchangeName, "topic", durable);
+		
+		final String queueName = "citybike-estaciones";
+		channel.queueDeclare(queueName, durable, false, false, null);
+		
+		String routingKey = "citybike.alquileres";
+		channel.queueBind(queueName, exchangeName, routingKey+".*");
+		
 		EventoAlquilerConcluido evento = new EventoAlquilerConcluido(idBici, LocalDateTime.now());
-		publicador.sendMessage(evento, ".alquiler-concluido");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonEvento = mapper.writeValueAsString(evento);
+		
+		channel.basicPublish(exchangeName, routingKey+"alquiler-concluido", new AMQP.BasicProperties.Builder()
+					.build(), jsonEvento.getBytes());
+		
+		channel.close();
+		connection.close();
 	}
 
 	@Override
-	public void publicarEventoBicicletaAlquilada(String idBici) {
+	public void publicarEventoBicicletaAlquilada(String idBici) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setUri("amqps://ppvihfpt:BDGv28w-UU11pgRbRFJTStZyrY0L77Yd@stingray.rmq.cloudamqp.com/ppvihfpt");
+
+		Connection connection = factory.newConnection();
+		
+		Channel channel = connection.createChannel();
+		
+		final String exchangeName = "citybike";
+		boolean durable = true;
+		channel.exchangeDeclare(exchangeName, "topic", durable);
+		
+		final String queueName = "citybike-estaciones";
+		channel.queueDeclare(queueName, durable, false, false, null);
+		
+		String routingKey = "citybike.alquileres";
+		channel.queueBind(queueName, exchangeName, routingKey+".*");
+		
 		EventoBicicletaAlquilada evento = new EventoBicicletaAlquilada(idBici, LocalDateTime.now());
-		publicador.sendMessage(evento, ".bicicleta-alquilada");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonEvento = mapper.writeValueAsString(evento);
+		
+		channel.basicPublish(exchangeName, routingKey+"alquiler-concluido", new AMQP.BasicProperties.Builder()
+					.build(), jsonEvento.getBytes());
+		
+		channel.close();
+		connection.close();
 	}
 
 	@Override
