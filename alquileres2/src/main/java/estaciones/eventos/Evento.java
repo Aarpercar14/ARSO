8daf5de25897b5c92a7bc7651e73499b20d0c7cc
sergiosanc16 <sergiosanchezillan@gmail.com>
2024-04-11 19:14:51 +0200

    nada

--- alquileres2/src/main/java/estaciones/eventos/Evento.java
+++ alquileres2/src/main/java/estaciones/eventos/Evento.java
@@ -3,7 +3,8 @@ package estaciones.eventos;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class Evento{
+public class Evento implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private String idBicicleta;
 	private LocalDateTime fecha;
 	
